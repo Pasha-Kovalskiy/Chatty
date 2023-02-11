932879f63bcdb4bb10cf8b2cdd82dd4d1c976c1a
Pasha Kovalskiy <kovalskyi08@gmail.com>
2023-02-11 15:45:23 +0200

    Added base webpack configuration

---
+++ frontend/webpack.common.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    filename: 'bundle.[fullhash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              ['@babel/preset-react', { runtime: 'automatic' }],
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(sc|sa|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'public', 'index.html'),
+      minify: false,
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.[fullhash].css',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
